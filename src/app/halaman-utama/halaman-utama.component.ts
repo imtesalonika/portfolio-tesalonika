867b6a9197e0af9ead71a6c8d8e8034c4627409e
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-06-12 15:57:00 +0700

    fixing bug

--- src/app/halaman-utama/halaman-utama.component.ts
+++ src/app/halaman-utama/halaman-utama.component.ts
@@ -1,9 +1,12 @@
   import { Component } from '@angular/core';
+  import {NavbarComponent} from "../navbar/navbar.component";
 
   @Component({
     selector: 'app-halaman-utama',
     standalone: true,
-    imports: [],
+    imports: [
+      NavbarComponent
+    ],
     templateUrl: './halaman-utama.component.html',
     styleUrl: './halaman-utama.component.css'
   })
